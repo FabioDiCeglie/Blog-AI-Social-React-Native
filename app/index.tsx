4f8e1a99f0a1a496ed1b8b3d914879fc939ec26b
FabioDiCeglie <fabiod@backbase.com>
2024-07-06 14:28:44 +0200

    feat: load all the fonts correctly and test it

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { Link } from 'expo-router';
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Homepage</Text>
+      <Text className="text-3xl font-pblack">Homepage</Text>
       <Link href={'/profile'} style={{ color: 'blue'}} >Go to profile!</Link>
     </View>
   );
