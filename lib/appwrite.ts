7bd42dd50ccda75f198e789eb5cae5b542601f96
FabioDiCeglie <fabiod@backbase.com>
2024-07-08 20:12:00 +0200

    feat: functionality get current user session information

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { Client, Account, ID, Avatars, Databases } from 'react-native-appwrite';
+import { Client, Account, ID, Avatars, Databases, Query } from 'react-native-appwrite';
 
 export const config = {
   endpoint: 'https://cloud.appwrite.io/v1',
@@ -30,7 +30,7 @@ export const createUser = async (email: string, password: string, username: stri
       password,
       username
     );
- 
+
     if (!newAccount) throw new Error();
 
     const avatarUrl = avatars.getInitials(username);
@@ -61,10 +61,31 @@ export const signIn = async (email: string, password: string) => {
     const session = await account.createEmailPasswordSession(email, password);
 
     if (!session) throw new Error('Session creation failed');
-    
+
     return session;
   } catch (error: any) {
     console.log(error);
     throw new Error(error);
   }
 };
+
+export const getCurrentUser = async () => {
+  try {
+    const currentAccount = await account.get()
+
+    if (!currentAccount) throw Error;
+
+    const currentUser = await databases.listDocuments(
+      config.databaseId,
+      config.usersCollectionId,
+      [Query.equal("accountId", currentAccount.$id)]
+    );
+
+    if (!currentUser) throw Error;
+
+    return currentUser.documents[0];
+  } catch (error: any) {
+    console.log(error);
+    throw new Error(error);
+  }
+}
