0da1e899c7cc8c5d97c01ccb4fde41a6a3fa9afe
FabioDiCeglie <fabiod@backbase.com>
2024-07-09 18:24:37 +0200

    feat: create empty state component if data are missing in the homepage

---
+++ components/EmptyState.tsx
@@ -0,0 +1,34 @@
+import { images } from '@/constants';
+import { router } from 'expo-router';
+import { Image, Text, View } from 'react-native';
+import CustomButton from './CustomButton';
+
+type EmptyStateProps = {
+  title: string;
+  subtitle: string;
+};
+
+const EmptyState = ({ title, subtitle }: EmptyStateProps) => {
+  return (
+    <View className='justify-center items-center px-4'>
+      <Image
+        source={images.empty}
+        className='w-[270px] h-[215px]'
+        resizeMode='contain'
+      />
+
+      <Text className='text-xl text-center font-psemibold text-white mt-2'>
+        {title}
+      </Text>
+      <Text className='font-pmedium text-sm text-gray-100'>{subtitle}</Text>
+
+      <CustomButton
+        title='Create video'
+        handlePress={() => router.push('/create')}
+        containerStyles='w-full my-5'
+      />
+    </View>
+  );
+};
+
+export default EmptyState;
